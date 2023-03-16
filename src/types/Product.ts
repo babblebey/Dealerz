1e21c2f1350c38c34a74306e948490b95bc852d9
babblebey <babblebey@gmail.com>
2023-03-16 10:07:27 +0100

    Added product response interface to types

---
+++ src/types/Product.ts
@@ -0,0 +1,16 @@
+export default interface Products {
+    id:          number;
+    title:       string;
+    price:       number;
+    description: string;
+    category:    Category;
+    image:       string;
+    rating:      Rating;
+}
+
+export type Category = "men's clothing" | "jewelery" | "electronics" | "women's clothing";
+
+export interface Rating {
+    rate:  number;
+    count: number;
+}
