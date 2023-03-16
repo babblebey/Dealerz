ae753ea2096e050792159d86122e5cf7d6aba45c
babblebey <babblebey@gmail.com>
2023-03-16 16:33:32 +0100

    configured app routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./app/store";
+import "./index.css";
+import Root from "./routes/root";
+import Shop from "./routes/shop";
+import Cart from "./routes/cart";
+import ProductDetails from "./routes/productDetails";
+import Home from "./routes/home";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Root />,
+        children: [
+            { path: '/', element: <Home /> },
+            { path: '/shop', element: <Shop /> },
+            { path: '/cart', element: <Cart /> },
+            { path: '/product/:productId', element: <ProductDetails /> }
+        ]
+    }
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <Provider store={ store }>
+            <RouterProvider router={ router } />
+        </Provider>
+    </React.StrictMode>
 )
