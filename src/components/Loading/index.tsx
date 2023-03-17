e2083d8393424aaf1209feea520ffa6aec492797
babblebey <babblebey@gmail.com>
2023-03-17 19:13:03 +0100

    improved component

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,12 +1,16 @@
 import { FC } from "react";
 
 interface LoadingProps {
-    
+    variant: 'full' | 'mini'
+    className?: string
 }
  
-const Loading: FC<LoadingProps> = () => {
+const Loading: FC<LoadingProps> = ({ variant, className }) => {
+    const full: boolean = variant === "full";
+    const mini: boolean = variant === "mini";
+
     return ( 
-        <div className="w-full min-h-[60vh] flex items-center justify-center">
+        <div className={`w-full ${ full ? 'min-h-[60vh]' : 'min-h-50' } ${ className } flex items-center justify-center`}>
             <div className="w-20 h-20 rounded-full border-4 border-gray-200 border-t-dorange-light animate-spin" />
         </div>
     );
