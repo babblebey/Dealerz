ecce00f33fe18d9af7de2ba7f0f1c2a59a8dc869
Olabode Lawal-Shittabey <babblebey@gmail.com>
2023-05-18 12:11:09 +0100

    Update root.tsx

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -11,7 +11,7 @@ const Root: FC<RootProps> = ({  }) => {
     return ( 
         <>
             <Navbar />
-                <Outlet />
+            <Outlet />
             <Footer />
         </>
     );
